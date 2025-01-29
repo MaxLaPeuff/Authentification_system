5415bd5d68d88f117a9cc28eb614b0cb47c208a7
MaxLaPeuff <maxaraye18@gmail.com>
2025-01-29 08:08:55 +0100

    Réglage 1

--- authentification/settings.py
+++ authentification/settings.py
@@ -37,7 +37,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-bt+ftkzsy&wg4c8t997^6i=ow&-#(ls(8s21(0!z36uyn4-^=)')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG','True')=="True"
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
